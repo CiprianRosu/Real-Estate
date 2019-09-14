f5418676c2d000c7195d845c083cff09b3e79410
Ciprian Rosu <cip.rosu@yahoo.com>
2019-09-14 15:52:39 +0200

    code

--- api/signUp.php
+++ api/signUp.php
@@ -2,15 +2,24 @@
 if( $_POST ){
   print_r($_POST);
   // TODO: validate name
-  echo("test");
-  $sName = $_POST['txtName'];
-  $jAgent = new stdClass();
-  $jAgent->name = $sName;
-  $jAgent->properties = new stdClass();
-  $sAgentUniqueId = uniqid();
+  $sUsername = $_POST['username'];
+  $sEmail = $_POST['email'];
+  $sPassword = $_POST['password'];
+  $sRePassword = $_POST['rePassword'];
+  $sAccountType = $_POST['accountType'];
+  $jUser = new stdClass();
+  $jUser->Username = $sUsername;
+  $jUser->Email = $sEmail;
+  $jUser->Password = $sPassword;
+  $jUser->AccountType = $sAccountType;
+  print_r($jUser);
+  if($sAccountType == "agent"){
+    $jUser->properties = new stdClass();
+  }
+  $sUniqueId = uniqid();
   $sjData = file_get_contents('data.json');
   $jData = json_decode( $sjData );
-  $jData->agents->$sAgentUniqueId = $jAgent;
+  $jData->user->$sUniqueId = $jUser;
   $sjData = json_encode( $jData, JSON_PRETTY_PRINT );
   file_put_contents( 'data.json', $sjData );
 }
